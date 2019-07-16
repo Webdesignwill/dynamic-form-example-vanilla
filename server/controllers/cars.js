029cff65d897a97e0fcfddfb761b2da551b49af8
Will Falkowski <will@webdesignwill.io>
2019-07-17 01:15:40 +0200

    Ading first collection

--- server/controllers/cars.js
+++ server/controllers/cars.js
@@ -1,36 +1,36 @@
-// let CarsModel = require('../models/cars')
-//
-// module.exports.allBrands = function(req, res) {
-//   CarsModel.find({}, (err, cars) => {
-//     if (err) res.send(err)
-//
-//     let brands = []
-//     cars.forEach((car) => {
-//       brands.push({
-//         _id: car._id,
-//         text: car.brand
-//       })
-//     })
-//
-//     res.json(brands)
-//   })
-// }
-//
-// module.exports.modelsByBrand = function(req, res) {
-//   CarsModel.findById(req.query.brand_id, (err, models) => {
-//     if (err) res.send(err)
-//
-//     let resModels = []
-//     models.models.forEach((model, index) => {
-//       resModels.push({
-//         _id: index,
-//         text: model
-//       })
-//     })
-//
-//     res.json(resModels)
-//   })
-// }
+let CarsModel = require('../models/cars')
+
+module.exports.allBrands = function(req, res) {
+  CarsModel.find({}, (err, cars) => {
+    if (err) res.send(err)
+
+    let brands = []
+    cars.forEach((car) => {
+      brands.push({
+        _id: car._id,
+        text: car.brand
+      })
+    })
+
+    res.json(brands)
+  })
+}
+
+module.exports.modelsByBrand = function(req, res) {
+  CarsModel.findById(req.query.brand_id, (err, models) => {
+    if (err) res.send(err)
+
+    let resModels = []
+    models.models.forEach((model, index) => {
+      resModels.push({
+        _id: index,
+        text: model
+      })
+    })
+
+    res.json(resModels)
+  })
+}
 
 const carsJson = [
   {
